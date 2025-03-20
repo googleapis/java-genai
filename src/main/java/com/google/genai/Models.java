294ed5c55b937e76f537908b93985f429b57f9cd
Google Team Member <genai-sdk-bot@google.com>
2025-03-20 11:34:16 -0700

    No public description
    PiperOrigin-RevId: 738880181

--- src/main/java/com/google/genai/Models.java
+++ src/main/java/com/google/genai/Models.java
@@ -252,10 +252,6 @@ public final class Models {
       throw new Error("maxLength parameter is not supported in Gemini API.");
     }
 
-    if (!Common.isZero(Common.getValueByPath(fromObject, new String[] {"title"}))) {
-      throw new Error("title parameter is not supported in Gemini API.");
-    }
-
     if (!Common.isZero(Common.getValueByPath(fromObject, new String[] {"minLength"}))) {
       throw new Error("minLength parameter is not supported in Gemini API.");
     }
@@ -359,6 +355,13 @@ public final class Models {
           Common.getValueByPath(fromObject, new String[] {"required"}));
     }
 
+    if (Common.getValueByPath(fromObject, new String[] {"title"}) != null) {
+      Common.setValueByPath(
+          toObject,
+          new String[] {"title"},
+          Common.getValueByPath(fromObject, new String[] {"title"}));
+    }
+
     if (Common.getValueByPath(fromObject, new String[] {"type"}) != null) {
       Common.setValueByPath(
           toObject,
@@ -397,13 +400,6 @@ public final class Models {
           toObject,
           new String[] {"maxLength"},
           Common.getValueByPath(fromObject, new String[] {"maxLength"}));
-    }
-
-    if (Common.getValueByPath(fromObject, new String[] {"title"}) != null) {
-      Common.setValueByPath(
-          toObject,
-          new String[] {"title"},
-          Common.getValueByPath(fromObject, new String[] {"title"}));
     }
 
     if (Common.getValueByPath(fromObject, new String[] {"minLength"}) != null) {
@@ -516,6 +512,13 @@ public final class Models {
           toObject,
           new String[] {"required"},
           Common.getValueByPath(fromObject, new String[] {"required"}));
+    }
+
+    if (Common.getValueByPath(fromObject, new String[] {"title"}) != null) {
+      Common.setValueByPath(
+          toObject,
+          new String[] {"title"},
+          Common.getValueByPath(fromObject, new String[] {"title"}));
     }
 
     if (Common.getValueByPath(fromObject, new String[] {"type"}) != null) {
