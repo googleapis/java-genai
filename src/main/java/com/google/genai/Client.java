a238a16843a303adf197558b69d665f49df159bd
Alejandro Cruzado-Ruiz <liebestraum@google.com>
2025-06-20 15:06:11 -0700

    chore: Refactor GenAI SDK to use OkHttp in place of Apache HttpClient
    Main gain of this move is to open the door for using the SDK on Android
    
    PiperOrigin-RevId: 773856442

--- src/main/java/com/google/genai/Client.java
+++ src/main/java/com/google/genai/Client.java
@@ -20,13 +20,12 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
-import com.google.genai.errors.GenAiIOException;
 import com.google.genai.types.ClientOptions;
 import com.google.genai.types.HttpOptions;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import okhttp3.OkHttpClient;
 
 /** Client class for GenAI. */
 public final class Client implements AutoCloseable {
@@ -368,11 +367,9 @@ public final class Client implements AutoCloseable {
   /** Closes the Client instance together with its instantiated http client. */
   @Override
   public void close() {
-    try {
-      apiClient.httpClient().close();
-    } catch (IOException e) {
-      throw new GenAiIOException("Failed to close the HTTP client.", e);
-    }
+    OkHttpClient httpClient = apiClient.httpClient();
+    httpClient.dispatcher().executorService().shutdown();
+    httpClient.connectionPool().evictAll();
   }
 
   /**
