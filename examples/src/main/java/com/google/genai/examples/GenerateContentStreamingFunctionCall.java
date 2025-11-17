3f0076a2cfed33f0b93868389485c611da95e0a8
Wanlin Du <wanlindu@google.com>
2025-11-17 15:14:35 -0800

    chore: add test for streaming function call for all languages
    PiperOrigin-RevId: 833515203

---
+++ examples/src/main/java/com/google/genai/examples/GenerateContentStreamingFunctionCall.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Usage:
+ *
+ * <p>1a. If you are using Vertex AI, setup ADC to get credentials:
+ * https://cloud.google.com/docs/authentication/provide-credentials-adc#google-idp
+ *
+ * <p>Then set Project, Location, and USE_VERTEXAI flag as environment variables:
+ *
+ * <p>export GOOGLE_CLOUD_PROJECT=YOUR_PROJECT
+ *
+ * <p>export GOOGLE_CLOUD_LOCATION=YOUR_LOCATION
+ *
+ * <p>export GOOGLE_GENAI_USE_VERTEXAI=true
+ *
+ * <p>1b. If you are using Gemini Developer API, set an API key environment variable. You can find a
+ * list of available API keys here: https://aistudio.google.com/app/apikey
+ *
+ * <p>export GOOGLE_API_KEY=YOUR_API_KEY
+ *
+ * <p>2. Compile the java package and run the sample code.
+ *
+ * <p>mvn clean compile exec:java
+ * -Dexec.mainClass="com.google.genai.examples.GenerateContentStreamingFunctionCall"
+ * -Dexec.args="YOUR_MODEL_ID"
+ */
+package com.google.genai.examples;
+
+import com.google.genai.Client;
+import com.google.genai.ResponseStream;
+import com.google.genai.types.FunctionCallingConfig;
+import com.google.genai.types.GenerateContentConfig;
+import com.google.genai.types.GenerateContentResponse;
+import com.google.genai.types.Tool;
+import com.google.genai.types.ToolConfig;
+import java.lang.reflect.Method;
+
+/** An example of using the Unified Gen AI Java SDK to generate content. */
+public final class GenerateContentStreamingFunctionCall {
+  /** A callable function to get the current weather. */
+  public static String getCurrentWeather(String location) {
+    return "The weather in " + location + " is " + "very nice.";
+  }
+
+  public static void main(String[] args) throws NoSuchMethodException {
+    final String modelId;
+    if (args.length != 0) {
+      modelId = args[0];
+    } else {
+      modelId = Constants.GEMINI_MODEL_NAME;
+    }
+
+    // Instantiate the client. The client by default uses the Gemini Developer API. It gets the API
+    // key from the environment variable `GOOGLE_API_KEY`. Vertex AI API can be used by setting the
+    // environment variables `GOOGLE_CLOUD_LOCATION` and `GOOGLE_CLOUD_PROJECT`, as well as setting
+    // `GOOGLE_GENAI_USE_VERTEXAI` to "true".
+    //
+    // Note: Some services are only available in a specific API backend (Gemini or Vertex), you will
+    // get a `UnsupportedOperationException` if you try to use a service that is not available in
+    // the backend you are using.
+    Client client = new Client();
+
+    if (client.vertexAI()) {
+      System.out.println("Using Vertex AI");
+    } else {
+      System.out.println("Gemini Developer API does not support streaming function calling.");
+      return;
+    }
+
+    // Add the  methods as callable functions to the tool.
+    Method method1 =
+        GenerateContentStreamingFunctionCall.class.getDeclaredMethod(
+            "getCurrentWeather", String.class);
+
+    GenerateContentConfig config =
+        GenerateContentConfig.builder()
+            .tools(Tool.builder().functions(method1))
+            .toolConfig(
+                ToolConfig.builder()
+                    .functionCallingConfig(
+                        FunctionCallingConfig.builder().streamFunctionCallArguments(true)))
+            .build();
+
+    ResponseStream<GenerateContentResponse> responseStream =
+        client.models.generateContentStream(
+            modelId, "What is the weather in San Francisco?", config);
+
+    System.out.println("Streaming response: ");
+    for (GenerateContentResponse res : responseStream) {
+      // Gets the function calls from the response by the quick accessor method `text()`.
+      System.out.print(res.functionCalls());
+
+      // Gets the http headers from the response.
+      res.sdkHttpResponse()
+          .ifPresent(
+              httpResponse ->
+                  System.out.println("Response headers: " + httpResponse.headers().orElse(null)));
+    }
+
+    // To save resources and avoid connection leaks, it is recommended to close the response
+    // stream after consumption (or using try block to get the response stream).
+    responseStream.close();
+  }
+
+  private GenerateContentStreamingFunctionCall() {}
+}
