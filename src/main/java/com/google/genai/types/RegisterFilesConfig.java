473324dc212d39f8f8915ac571166d0b392c089c
Mark Daoust <markdaoust@google.com>
2026-01-29 13:48:46 -0800

    feat: Add `registerFiles` for Java.
    Register files is a GenAi Only feature (no Vertex).
    GenAi Doesn't support GoogleCredentials.
    The register files method needs a GoogleCredentials token, so the user has to pass GoogleCredentials to the `registerFiles` method.
    
    PiperOrigin-RevId: 862884363

---
+++ src/main/java/com/google/genai/types/RegisterFilesConfig.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Auto-generated code. Do not edit.
+
+package com.google.genai.types;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.google.auto.value.AutoValue;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import com.google.genai.JsonSerializable;
+import java.util.Optional;
+
+/** Used to override the default configuration. */
+@AutoValue
+@JsonDeserialize(builder = RegisterFilesConfig.Builder.class)
+public abstract class RegisterFilesConfig extends JsonSerializable {
+  /** Used to override HTTP request options. */
+  @JsonProperty("httpOptions")
+  public abstract Optional<HttpOptions> httpOptions();
+
+  /** If true, the raw HTTP response will be returned in the 'sdk_http_response' field. */
+  @JsonProperty("shouldReturnHttpResponse")
+  public abstract Optional<Boolean> shouldReturnHttpResponse();
+
+  /** Instantiates a builder for RegisterFilesConfig. */
+  @ExcludeFromGeneratedCoverageReport
+  public static Builder builder() {
+    return new AutoValue_RegisterFilesConfig.Builder();
+  }
+
+  /** Creates a builder with the same values as this instance. */
+  public abstract Builder toBuilder();
+
+  /** Builder for RegisterFilesConfig. */
+  @AutoValue.Builder
+  public abstract static class Builder {
+    /** For internal usage. Please use `RegisterFilesConfig.builder()` for instantiation. */
+    @JsonCreator
+    private static Builder create() {
+      return new AutoValue_RegisterFilesConfig.Builder();
+    }
+
+    /**
+     * Setter for httpOptions.
+     *
+     * <p>httpOptions: Used to override HTTP request options.
+     */
+    @JsonProperty("httpOptions")
+    public abstract Builder httpOptions(HttpOptions httpOptions);
+
+    /**
+     * Setter for httpOptions builder.
+     *
+     * <p>httpOptions: Used to override HTTP request options.
+     */
+    @CanIgnoreReturnValue
+    public Builder httpOptions(HttpOptions.Builder httpOptionsBuilder) {
+      return httpOptions(httpOptionsBuilder.build());
+    }
+
+    @ExcludeFromGeneratedCoverageReport
+    abstract Builder httpOptions(Optional<HttpOptions> httpOptions);
+
+    /** Clears the value of httpOptions field. */
+    @ExcludeFromGeneratedCoverageReport
+    @CanIgnoreReturnValue
+    public Builder clearHttpOptions() {
+      return httpOptions(Optional.empty());
+    }
+
+    /**
+     * Setter for shouldReturnHttpResponse.
+     *
+     * <p>shouldReturnHttpResponse: If true, the raw HTTP response will be returned in the
+     * 'sdk_http_response' field.
+     */
+    @JsonProperty("shouldReturnHttpResponse")
+    public abstract Builder shouldReturnHttpResponse(boolean shouldReturnHttpResponse);
+
+    @ExcludeFromGeneratedCoverageReport
+    abstract Builder shouldReturnHttpResponse(Optional<Boolean> shouldReturnHttpResponse);
+
+    /** Clears the value of shouldReturnHttpResponse field. */
+    @ExcludeFromGeneratedCoverageReport
+    @CanIgnoreReturnValue
+    public Builder clearShouldReturnHttpResponse() {
+      return shouldReturnHttpResponse(Optional.empty());
+    }
+
+    public abstract RegisterFilesConfig build();
+  }
+
+  /** Deserializes a JSON string to a RegisterFilesConfig object. */
+  @ExcludeFromGeneratedCoverageReport
+  public static RegisterFilesConfig fromJson(String jsonString) {
+    return JsonSerializable.fromJsonString(jsonString, RegisterFilesConfig.class);
+  }
+}
