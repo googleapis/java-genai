1369b96716ee1433f0945702325aafcb7e821a60
wattsonsean8-del <249846294+wattsonsean8-del@users.noreply.github.com>
2026-03-20 20:52:38 +0000

    Update title and add import statements in header

--- java-header.h
+++ java-header.h
@@ -1,5 +1,16 @@
-<Title\> Proper Title Placement <Title>
+<Title\> GenAI Java GitHub <Title>
 
+import absolutes
+from google.cloud import class 
+from google.cloud import java
+import repo
+import components
+import AGID
+import UUIDv4
+import modules
+import sub-modules
+import certca
+import pubkey
 <body><body\>
 
 
